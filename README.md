c9f590512e40333bde26abb5d3f6582606925184
Dewey Dunnington <dewey@fishandwhistle.net>
2020-02-09 12:50:45 -0400

    format front matter

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-This is a minimal example of a book based on R Markdown and **bookdown** (https://github.com/rstudio/bookdown). Please see the page "Get Started" at https://bookdown.org/ for how to compile this example.
+
+# Physical Geology Sources
+
+_Physical Geology_ is a comprehensive introductory text on the physical aspects of geology, including rocks and minerals, plate tectonics, earthquakes, volcanoes, mass wasting, climate change, planetary geology and much more. It has a strong emphasis on examples from western Canada. It is adapted from ["Physical Geology"](https://opentextbc.ca/geology/) written by Steven Earle for the BCcampus Open Textbook Program, and ["Physical Geology, First University of Saskatchewan Edition"](https://openpress.usask.ca/physicalgeology/) by Karla Panchuk. This repository is a **bookdown** (https://github.com/rstudio/bookdown) adaptation of these projects to facilitate editing and sharing of this amazing project. Please see the page "Get Started" at https://bookdown.org/ for how to compile this source into an actual book (website, PDF, etc.).
+
+## Copyright
+
+Unless otherwise noted, this book is released under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 License](https://creativecommons.org/licenses/by-nc-sa/4.0/) also known as a CC BY-NC-SA 4.0 license. This means you are free to copy, redistribute, remix, transform, and build upon this book, as long as you share derivative materials using a CC BY-NC-SA 4.0 license. Under this license, anyone who redistributes this textbook can do so for free providing they properly attribute the book as follows:
+
+> [Physical Geology, First University of Saskatchewan Edition](https://physicalgeology.pressbooks.com) by Karla Panchuk used under a [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/) international license.
+
+Additionally, if you redistribute this textbook, in whole or in part, in either a print or digital format, then you must retain on every physical and/or electronic page the following attribution:
+
+> https://openpress.usask.ca/physicalgeology/
--- data-raw/collect-bookdown.R
+++ data-raw/collect-bookdown.R
@@ -1,6 +1,7 @@
 
 library(tidyverse)
 library(xml2)
+library(rvest)
 
 root <- read_xml("data-raw/Physical-Geology-First-University-of-Saskatchewan-Edition-1547781813.xml")
 
@@ -12,6 +13,7 @@ items <- tibble(
   type = map_chr(item, . %>% xml_find_first("./wp:post_type") %>% xml_text()),
   url = map_chr(item, . %>% xml_find_first("./wp:attachment_url") %>% xml_text()),
   status = map_chr(item, . %>% xml_find_first("./wp:status") %>% xml_text()),
+  order = as.numeric(map_chr(item, . %>% xml_find_first("./wp:menu_order") %>% xml_text())),
   content = map_chr(item, . %>% xml_find_first("./content:encoded") %>% xml_text()),
 )
 
@@ -24,10 +26,14 @@ chapter_titles <- items %>%
   extract(title, c("chapter", "chapter_title"), "Chapter\\s+([0-9]+)\\s*\\.\\s*(.*)") %>% 
   filter(!is.na(chapter)) %>% 
   mutate(chapter = as.numeric(chapter)) %>% 
-  print()
+  arrange(chapter)
+
+front_matter <- items %>% 
+  filter(type == "front-matter", status != "trash", title != "Download PDF Files") %>% 
+  arrange(order)
 
 chapter_content <- items %>% 
-  filter(type == "chapter") %>% 
+  filter(type == "chapter", status != "trash") %>% 
   select(-url, -type, -item) %>% 
   extract(title, c("chapter", "section"), "([0-9]+)\\.([0-9X]+)", remove = FALSE) %>% 
   extract(title, c("chapter2", "section2"), "Chapter\\s+([0-9]+)\\s+(.*)", remove = FALSE) %>% 
@@ -37,8 +43,112 @@ chapter_content <- items %>%
       factor(levels = c(1:20, "X", "Summary", "Review Questions"))
   ) %>% 
   arrange(chapter, section) %>% 
-  select(-chapter2, -section2) %>% 
-  print()
+  select(-chapter2, -section2)
 
+
+# html->md filter functions
+md_filter_links <- function(x) {
+  str_replace_all(
+    x,
+    "<a\\s+.*?href\\s*=\\s*(['\"])(.*?)\\1.*?>(.*?)</a>", 
+    "[\\3](\\2)"
+  )
+}
+
+md_filter_bold <- function(x) {
+  str_replace_all(
+    x,
+    "<(strong|b)>(.*?)</\\1>", 
+    "__\\2__"
+  )
+}
+
+# in this book <h1> is used as a first heading
+# on individual pages. in bookdown, this needs to be
+# a ##
+md_filter_hn <- function(x) {
+  matches <- str_match_all(
+    x,
+    "<h([0-9])>(.*?)</h\\1>"
+  )[[1]]
+  headings <- strrep("#", as.numeric(matches[, 2]) + 2)
+
+  for (i in seq_len(nrow(matches))) {
+    x <- str_replace(x, fixed(matches[i, 1]), paste0("\n", headings[i], " ", matches[i, 3], "\n\n"))
+  }
+  
+  x
+}
+
+md_filter_italics <- function(x) {
+  str_replace_all(
+    x,
+    "<(em|i)>(.*?)</\\1>", 
+    "_\\2_"
+  )
+}
+
+md_filter_textbox <- function(x) {
+  str_replace_all(
+    x,
+    regex("<div\\s+class=\"textbox\">(.*?)</div>", multiline = TRUE, dotall = TRUE), 
+    "\n```{block, type='textbox'}\n\\1\n```\n\n"
+  )
+}
+
+# will bork nested ULs, of which there are hopefully not too many
+md_filter_ul <- function(x) {
+  ul_items <- str_extract_all(
+    x,
+    regex("<ul>(.*?)</ul>", multiline = TRUE, dotall = TRUE)
+  ) %>% 
+    .[[1]] %>% 
+    unique()
+  
+  for (ul_item in ul_items) {
+    li_items <- read_html(ul_items) %>% 
+      html_nodes("li") %>% 
+      as.character() %>% 
+      str_replace("<li>(.*?)</li>", "\\1") %>% 
+      paste0("- ", ., collapse = "\n") %>% 
+      paste0("\n\n", ., "\n\n")
+    x <- str_replace_all(x, fixed(ul_item), li_items)
+  }
+  
+  x
+}
+
+md_filter_p <- function(x) {
+  str_replace_all(
+    x,
+    regex("<p>(.*?)</p>", multiline = TRUE, dotall = TRUE), 
+    "\n\n\\1\n\n"
+  )
+}
+
+md_filter_all <- function(x) {
+  if (length(x) > 1) {
+    return(map_chr(x, md_filter_all))
+  }
+  
+  x %>% 
+    md_filter_links() %>% 
+    md_filter_bold() %>% 
+    md_filter_italics() %>% 
+    md_filter_hn() %>% 
+    md_filter_textbox() %>% 
+    md_filter_ul() %>% 
+    md_filter_p()
+}
+
+# copy front matter so that it can be pasted into index.Rmd
+front_matter %>% 
+  mutate(
+    content_markdown = glue::glue("\n## {title}\n\n{md_filter_all(content)}")
+  ) %>% 
+  pull(content_markdown) %>% 
+  paste(collapse = "\n\n") %>% 
+  clipr::write_clip()
+  
 
 
